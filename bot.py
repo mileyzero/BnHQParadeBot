a940d359783277cf5589022f9bbc4056d8022f12
mileyzero <jerometan23000@hotmail.com>
2026-02-10 01:31:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -435,11 +435,7 @@ def main():
     app.add_handler(reg_handler)
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_buttons))
 
-    # Job for auto midnight reset
-    async def schedule_jobs(app):
-        app.job_queue.run_daily(midnight_reset, time=datetime.time(hour=0, minute=0))
-        
-    app.post_init.append(schedule_jobs)
+    app.job_queue.run_daily(midnight_reset, time=datetime.time(hour=0, minute=0))
 
     print("Bot running...")
     app.run_polling()
